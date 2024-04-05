5057818c8989b626913b79427f0d346048c39c78
v87278 <32817699+v87278@users.noreply.github.com>
2024-04-05 16:36:32 +0530

    Update pincode_search.py

--- pincode_search.py
+++ pincode_search.py
@@ -44,4 +44,4 @@ def get_data():
         return jsonify({'error': 'Pincode parameter is required'}), 400
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=8000)
