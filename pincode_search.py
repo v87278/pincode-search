83b8325e26e872d9803fdd8ec526738bcf5cc8ea
v87278 <32817699+v87278@users.noreply.github.com>
2024-04-05 16:19:57 +0530

    host updated

--- pincode_search.py
+++ pincode_search.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, request
 from pymongo import MongoClient
 from dotenv import load_dotenv
 import os
+from urllib.parse import quote_plus
 
 app = Flask(__name__)
 
@@ -9,21 +10,23 @@ app = Flask(__name__)
 load_dotenv()
 
 # Get MongoDB configuration from environment variables
-print(os.getenv('MONGO_URI'))
-print('---------------')
-print(os.getenv('DATABASE_NAME'))
-print('---------------')
-print(os.getenv('COLLECTION_NAME'))
-print('---------------')
-MONGO_URI = os.getenv('MONGO_URI')
+
+print(os.getenv('MONGO_USERNAME'), os.getenv('MONGO_PASSWORD'), os.getenv('MONGO_HOST'), os.getenv('MONGO_PORT'))
+
+MONGO_USERNAME = os.getenv('MONGO_USERNAME')
+MONGO_PASSWORD = os.getenv('MONGO_PASSWORD')
+MONGO_HOST = os.getenv('MONGO_HOST')
+MONGO_PORT = os.getenv('MONGO_PORT')
 DATABASE_NAME = os.getenv('DATABASE_NAME')
 COLLECTION_NAME = os.getenv('COLLECTION_NAME')
+
+# Construct MongoDB URI with properly escaped username and password
+MONGO_URI = f'mongodb://{quote_plus(MONGO_USERNAME)}:{quote_plus(MONGO_PASSWORD)}@{MONGO_HOST}:{MONGO_PORT}/'
 
 # Connect to MongoDB using configuration from environment variables
 client = MongoClient(MONGO_URI)
 db = client[DATABASE_NAME]
 collection = db[COLLECTION_NAME]
-
 
 @app.route('/get_data', methods=['GET'])
 def get_data():
@@ -39,7 +42,6 @@ def get_data():
             return jsonify({'error': 'Pincode not found'}), 404
     else:
         return jsonify({'error': 'Pincode parameter is required'}), 400
-
 
 if __name__ == '__main__':
     app.run(debug=True)
